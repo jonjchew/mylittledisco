9457dff38096605b6a8c93cd8f508ffc2bd5c482
srinivas rao <rao@64-71-17-228.static.wiline.com>
2013-11-08 23:16:03 -0800

    songs controller now communicates with sound cloud controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,11 @@ require 'modules/SoundCloud'
 class SongsController < ApplicationController
   include SoundCloud
 
+  def searchSoundCloud
+    results_array = search(params["query"])
+
+    render json: results_array
+  end
 end
+
+
