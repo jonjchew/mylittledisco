4b547f5325f25ad4815e0be736c90ffb71404a35
srinivas rao <rao@64-71-17-229.static.wiline.com>
2013-11-09 02:29:45 -0800

    search functionality moved into an object literal
    - IDs stored in dom, can be queried back.

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,40 +1,57 @@
 
 
 $(function() {
-  $('#search-bar').on('input',hitSoundCloud)
+  $('#search-bar').on('input',Search.hitSoundCloud)
 })
 
-var timeout
 
-var hitSoundCloud = function(inputEvent) {
-  clearTimeout(timeout)
-  var query = inputEvent.target.value
-  timeout = setTimeout(function() { getSoundCloudData(query) },1000)
-}
-
-var getSoundCloudData = function(queryString) {
-  $.ajax({
-    url: '/search',
-    type: 'POST',
-    data: {query: queryString},
-    success: appendResults
-  })
-}
-
-var appendResults = function(songsArray) {
-  clearResults()
-  for(var i = 0 ; i < songsArray.length;i++) {
-    appendSong(songsArray[i])
+var Search = {
+  songs: {},
+  hitSoundCloud: function(inputEvent) {
+    clearTimeout(Search.timeout)
+    var query = inputEvent.target.value
+    Search.timeout = setTimeout(function() { Search.getSoundCloudData(query) },1000)
+  },
+  getSoundCloudData: function(queryString) {
+    $.ajax({
+      url: '/search',
+      type: 'POST',
+      data: {query: queryString},
+      success: Search.appendResults
+    })
+  },
+  appendResults: function(songsArray) {
+    Search.clearResults()
+    for(var i = 0 ; i < songsArray.length;i++) {
+      Search.appendSong(songsArray[i])
+      Search.songs[songsArray[i].id] = songsArray[i]
+    }
+  },
+  clearResults: function() {
+    $('#results').empty()
+  },
+  appendSong: function(songHash) {
+    var song = $('#hidden .song').clone()
+    song.find('.song-title').text(songHash.title)
+    song.find('.add-song-button').val(songHash.id)
+    $('#results').append(song)
+  },
+  getSong: function(id) {
+    return songs[id]
   }
 }
 
-var clearResults = function() {
-  $('#results').empty()
-}
 
-var appendSong = function(songHash) {
-  var song = $('#hidden .song').clone()
-  song.find('.song-title').text(songHash.title)
-  song.find('.add-song-button').val(JSON.stringify(songHash))
-  $('#results').append(song)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
