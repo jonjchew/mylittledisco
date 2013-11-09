e0b7e0d6fbc2aa198dcb32ce847b581ac8f15826
Jonathan Chew <jon.j.chew@gmail.com>
2013-11-08 19:58:25 -0800

    created environment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mylittledisco::Application.config.secret_key_base = '80e816863d2c4f125d2324bd087dcbd95a2f6a99831f79538ded4340fa28b13b371af3a2d31341cf2034e090067fb9ae628c65033cb0499d63d42e43bafba3b1'
