e2961e0eb065fdd48636734aa8a76b1d4c18e41f
raorao <savinirs@gmail.com>
2013-11-09 16:59:55 -0800

    Rooms without users are automatically deleted

--- app/models/room.rb
+++ app/models/room.rb
@@ -7,4 +7,8 @@ class Room < ActiveRecord::Base
   def users_count
     WebsocketRails[name].subscribers.count
   end
+
+  def self.delete_unused
+    all.each {|room| room.destroy if room.users_count == 0}
+  end
 end
